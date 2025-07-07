00d150bf4e5c4cdab4ac26c0c69b6e137a224c9b
Kevin <kevinfagan608@gmail.com>
2025-07-07 17:36:57 -0500

    CHORE: link repository cleanup

--- internal/repository/links.go
+++ internal/repository/links.go
@@ -65,39 +65,19 @@ func (l *LinkRepository) GetLinks(search string, page, pageSize int) ([]model.Li
 		err   error
 	)
 
-	if search == "" {
-		err = l.withTx(func(tx *sql.Tx) error {
-			links, err = l.getLinksTx(tx, page, pageSize)
-			if err != nil {
-				return err
-			}
+	err = l.withTx(func(tx *sql.Tx) error {
+		links, err = l.getLinksTx(tx, page, pageSize, search)
+		if err != nil {
+			return err
+		}
 
-			count, err = l.countLinksTx(tx)
-			if err != nil {
-				return err
-			}
+		count, err = l.countLinkTx(tx, search)
+		if err != nil {
+			return err
+		}
 
-			return nil
-		})
-	} else {
-		err = l.withTx(func(tx *sql.Tx) error {
-			links, err = l.getLinksSearchTx(tx, search, page, pageSize)
-			if err != nil {
-				return err
-			}
-
-			count, err = l.countLinksSearchTx(tx, search)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		})
-	}
-
-	if err != nil {
-		return nil, 0, err
-	}
+		return nil
+	})
 
 	return links, count, nil
 }
@@ -120,48 +100,6 @@ func (l *LinkRepository) UpdateLink(short, long string) error {
 	})
 }
 
-func (l *LinkRepository) getLinksTx(tx *sql.Tx, page, pageSize int) ([]model.Link, error) {
-	var (
-		rows *sql.Rows
-		err  error
-	)
-
-	rows, err = tx.Query(`
-		SELECT short_url, long_url, visits, last_updated
-		FROM links
-		ORDER BY visits DESC
-		LIMIT ? OFFSET ?;`, pageSize, pageSize*page)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-	return l.iterateLinkRows(rows)
-}
-
-func (l *LinkRepository) getLinksSearchTx(tx *sql.Tx, search string, page, pageSize int) ([]model.Link, error) {
-	var (
-		rows *sql.Rows
-		err  error
-	)
-
-	pattern := "%" + search + "%"
-	rows, err = tx.Query(`
-			SELECT short_url, long_url, visits, last_updated
-			FROM links
-			WHERE short_url LIKE ? OR long_url LIKE ?
-			ORDER BY visits DESC
-			LIMIT ? OFFSET ?;`, pattern, pattern, pageSize, pageSize*page)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-	return l.iterateLinkRows(rows)
-}
-
 func (l *LinkRepository) withTx(fn func(tx *sql.Tx) error) error {
 	tx, err := l.sql.Begin()
 	if err != nil {
@@ -177,21 +115,63 @@ func (l *LinkRepository) withTx(fn func(tx *sql.Tx) error) error {
 	return tx.Commit()
 }
 
-func (l *LinkRepository) countLinksTx(tx *sql.Tx) (int, error) {
-	var count int
-	err := tx.QueryRow(`SELECT COUNT(*) FROM links;`).Scan(&count)
+func (l *LinkRepository) getLinksTx(tx *sql.Tx, page, pageSize int, search string) ([]model.Link, error) {
+	var (
+		rows *sql.Rows
+		err  error
+	)
 
-	return count, err
+	if search == "" {
+		rows, err = tx.Query(`
+		SELECT short_url, long_url, visits, last_updated
+		FROM links
+		ORDER BY visits DESC
+		LIMIT ? OFFSET ?;`, pageSize, pageSize*page)
+	} else {
+		pattern := "%" + search + "%"
+		rows, err = tx.Query(`
+			SELECT short_url, long_url, visits, last_updated
+			FROM links
+			WHERE short_url LIKE ? OR long_url LIKE ?
+			ORDER BY visits DESC
+			LIMIT ? OFFSET ?;`, pattern, pattern, pageSize, pageSize*page)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var links []model.Link
+	for rows.Next() {
+		var link model.Link
+		err := rows.Scan(&link.ShortURL, &link.LongURL, &link.Visits, &link.LastUpdated)
+		if err != nil {
+			return nil, err
+		}
+
+		links = append(links, link)
+	}
+
+	return links, nil
 }
 
-func (l *LinkRepository) countLinksSearchTx(tx *sql.Tx, search string) (int, error) {
+func (l *LinkRepository) countLinkTx(tx *sql.Tx, search string) (int, error) {
 	var count int
-	pattern := "%" + search + "%"
-	err := tx.QueryRow(`
+
+	if search == "" {
+		err := tx.QueryRow(`SELECT COUNT(*) FROM links;`).Scan(&count)
+		return count, err
+	} else {
+		pattern := "%" + search + "%"
+
+		err := tx.QueryRow(`
 		SELECT COUNT(*) FROM links
 		WHERE short_url LIKE ? OR long_url LIKE ?;`, pattern, pattern).Scan(&count)
 
-	return count, err
+		return count, err
+	}
 }
 
 func (l *LinkRepository) createLinkTx(tx *sql.Tx, short, long string) error {
@@ -248,20 +228,4 @@ func (l *LinkRepository) logAuditTx(tx *sql.Tx, short, long, action string) erro
 	VALUES (?, ?, ?);`, short, long, action)
 
 	return err
-}
-
-func (l *LinkRepository) iterateLinkRows(rows *sql.Rows) ([]model.Link, error) {
-	var links []model.Link
-
-	for rows.Next() {
-		var link model.Link
-		err := rows.Scan(&link.ShortURL, &link.LongURL, &link.Visits, &link.LastUpdated)
-		if err != nil {
-			return nil, err
-		}
-
-		links = append(links, link)
-	}
-
-	return links, nil
 }
